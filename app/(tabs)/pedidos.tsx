d1f6c75543b9de37d784c29b18cd59ba86812803
marifer2409 <mariafernandaarmijos3@gmail.com>
2026-09-24 03:13:02 +0000

    feet(04_pedidos) Completada

--- app/(tabs)/pedidos.tsx
+++ app/(tabs)/pedidos.tsx
@@ -10,28 +10,33 @@ export default function PedidosScreen() {
   // TODO [RETO 04 - PASO 1]: Declara dos estados simples usando useState:
   // 1. 'total' para el dinero acumulado (inicializado en 0)
   // 2. 'items' para la cantidad de productos (inicializado en 0)
-  // const [total, setTotal] = useState(0);
-  // const [items, setItems] = useState(0);
+   const [total, setTotal] = useState(0);
+   const [items, setItems] = useState(0);
 
   // TODO [RETO 04 - PASO 2]: Programa las funciones de suma directa al total y conteo de items:
   const agregarSanduche = () => {
-    // TODO: Sumar 1 a items y sumar 1.25 al total
+    setItems(items + 1);
+    setTotal(total + 1.25);
   };
 
   const agregarEmpanada = () => {
+    setItems(items + 1);
+    setTotal(total + 0.75);
     // TODO: Sumar 1 a items y sumar 0.75 al total
   };
 
   const agregarJugo = () => {
+    setItems(items + 1);
+    setTotal(total + 1.80);
     // TODO: Sumar 1 a items y sumar 0.80 al total
   };
 
   const vaciarCarrito = () => {
+    setItems(0);
+    setTotal(0);
     // TODO: Reiniciar total e items a 0
   };
 
-  const total = 0; // TODO: Conectar con tu estado useState 'total'
-  const items = 0; // TODO: Conectar con tu estado useState 'items'
   const tieneDescuento = total >= 5.0;
 
   return (
